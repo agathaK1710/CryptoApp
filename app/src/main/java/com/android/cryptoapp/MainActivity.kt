549db1b40b7a51ceca923bdb7ff2f198d1471177
Agatha <agatha.kazak@gmail.com>
2022-04-22 22:54:03 +0300

    add network(apiService and apiFactory)

--- app/src/main/java/com/android/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/android/cryptoapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.android.cryptoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.cryptoapp.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                    Log.d("TEST_OF_LOADING_DATA", it.toString())
+            },{
+                it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
---
+++ app/src/main/java/com/android/cryptoapp/api/ApiService.kt
@@ -0,0 +1,33 @@
+package com.android.cryptoapp.api
+
+import com.android.cryptoapp.pojo.CoinInfoListOfData
+import com.android.cryptoapp.pojo.CoinPriceInfoRawData
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = API_KEY,
+        @Query(QUERY_PARAM_TSYMS) tsyms: String = CURRENCY,
+        @Query(QUERY_PARAM_FSYMS) fsyms: String
+    ): Single<CoinPriceInfoRawData>
+
+    @GET("top/totalvolfull")
+    fun getTopCoinsInfo(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = API_KEY,
+        @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
+        @Query(QUERY_PARAM_TSYM) tsym: String = CURRENCY
+    ): Single<CoinInfoListOfData>
+
+    companion object {
+        private const val API_KEY = "3d225ebf8b3653dfa41a94694185064565a7acb233915bda48ae13ecdd659a0c"
+        private const val CURRENCY = "USD"
+        private const val QUERY_PARAM_API_KEY = "api_key"
+        private const val QUERY_PARAM_FSYMS = "fsyms"
+        private const val QUERY_PARAM_TSYMS = "tsyms"
+        private const val QUERY_PARAM_LIMIT = "limit"
+        private const val QUERY_PARAM_TSYM = "tsym"
+    }
+}
